936658a9d08991f422853dff10c52101e00b12e2
santosh <palsantosh1698@gmail.com>
2024-10-09 04:15:32 -0700

    minor chages cors

--- index.js
+++ index.js
@@ -13,32 +13,31 @@ require("./src/db/clusterdb_server");
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
-
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const csrf = require("csurf");
 
-app.use(cors({
-  origin: `${process.env.API_PROd_BASEURL}`, // Replace with your frontend domain
-  optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",`${process.env.API_PROd_BASEURL}`
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, access_token"
-//   );
-//   if ("OPTIONS" == req.method) {
-//     res.send(200);
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// });
+  next();
+});
 
 const csrfProtection = csrf();
 app.use(bodyParser.urlencoded({ extended: false }));
