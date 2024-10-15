551f6b30d4d47407808b124a3a6ba369b772959c
santosh <palsantosh1698@gmail.com>
2024-10-15 14:22:55 +0530

    add new changes

--- swager.js
+++ swager.js
@@ -15,8 +15,8 @@ const outputFile = "./swagger-output.json";
 const routes = [
   "./src/routes/Register.js",
   "./src/routes/customer_view.js",
-  // "./src/routes/employee_view.js",
-  // "./src/routes/student.js",
+  "./src/routes/employee_view.js",
+  "./src/routes/student.js",
 ];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
